e064e0635f32ce39cad25e0924038d06b320a74b
sgalinsk <sgalinsk@735d13b6-9817-0410-8766-e36946ffe9aa>
2009-06-26 16:44:07 +0000

    released version 2.6.5
    git-svn-id: https://svn.typo3.org/TYPO3v4/Extensions/scriptmerger/trunk@21821 735d13b6-9817-0410-8766-e36946ffe9aa

--- ext_emconf.php
+++ ext_emconf.php
@@ -3,7 +3,7 @@
 ########################################################################
 # Extension Manager/Repository config file for ext: "scriptmerger"
 #
-# Auto generated 26-06-2009 17:18
+# Auto generated 26-06-2009 18:43
 #
 # Manual updates:
 # Only the data in the array - anything else is removed by next write.
@@ -15,7 +15,7 @@ $EM_CONF[$_EXTKEY] = array(
 	'description' => 'You need more speed? Then this extension is made for you! It minimizes the http requests by merging of css and javascript files. Additionaly it offers an image to cdata conversion.',
 	'category' => 'fe',
 	'shy' => 0,
-	'version' => '2.6.4',
+	'version' => '2.6.5',
 	'dependencies' => '',
 	'conflicts' => '',
 	'priority' => 'bottom',
@@ -35,14 +35,14 @@ $EM_CONF[$_EXTKEY] = array(
 	'constraints' => array(
 		'depends' => array(
 			'php' => '5.0.0-5.2.99',
-			'typo3' => '4.2.0-4.3.99'
+			'typo3' => '4.2.0-4.3.99',
 		),
 		'conflicts' => array(
 		),
 		'suggests' => array(
 		),
 	),
-	'_md5_values_when_last_written' => 'a:11:{s:25:"class.tx_scriptmerger.php";s:4:"87e8";s:21:"ext_conf_template.txt";s:4:"0557";s:12:"ext_icon.gif";s:4:"2cfb";s:17:"ext_localconf.php";s:4:"3840";s:31:"resources/jsmin/jsmin-1.1.1.php";s:4:"dc0c";s:35:"resources/csstidy/class.csstidy.php";s:4:"7def";s:41:"resources/csstidy/class.csstidy_ctype.php";s:4:"ad91";s:44:"resources/csstidy/class.csstidy_optimise.php";s:4:"0990";s:41:"resources/csstidy/class.csstidy_print.php";s:4:"499a";s:30:"resources/csstidy/data.inc.php";s:4:"5164";s:14:"doc/manual.sxw";s:4:"1908";}',
+	'_md5_values_when_last_written' => 'a:11:{s:25:"class.tx_scriptmerger.php";s:4:"0fa0";s:21:"ext_conf_template.txt";s:4:"0557";s:12:"ext_icon.gif";s:4:"2cfb";s:17:"ext_localconf.php";s:4:"3840";s:31:"resources/jsmin/jsmin-1.1.1.php";s:4:"dc0c";s:35:"resources/csstidy/class.csstidy.php";s:4:"7def";s:41:"resources/csstidy/class.csstidy_ctype.php";s:4:"ad91";s:44:"resources/csstidy/class.csstidy_optimise.php";s:4:"0990";s:41:"resources/csstidy/class.csstidy_print.php";s:4:"499a";s:30:"resources/csstidy/data.inc.php";s:4:"5164";s:14:"doc/manual.sxw";s:4:"1908";}',
 	'suggests' => array(
 	),
 );
