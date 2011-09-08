41581c59d61dedb3a9bc14eac61690fdc41fe6f2
sgalinsk <sgalinsk@735d13b6-9817-0410-8766-e36946ffe9aa>
2011-09-08 14:06:40 +0000

    [TASK] Release of version 3.2.0
    git-svn-id: https://svn.typo3.org/TYPO3v4/Extensions/scriptmerger/trunk@51762 735d13b6-9817-0410-8766-e36946ffe9aa

--- ext_emconf.php
+++ ext_emconf.php
@@ -3,7 +3,7 @@
 ########################################################################
 # Extension Manager/Repository config file for ext "scriptmerger".
 #
-# Auto generated 21-10-2010 23:35
+# Auto generated 08-09-2011 16:06
 #
 # Manual updates:
 # Only the data in the array - everything else is removed by next
@@ -15,7 +15,7 @@ $EM_CONF[$_EXTKEY] = array(
 	'description' => 'This extension minimizes the http requests by concatenating your css and javascript. Furthermore the result can be minified and compressed. This whole process is highly configurable and is partly based on the "minify", "jsminplus" and "jsmin" projects.',
 	'category' => 'fe',
 	'shy' => 0,
-	'version' => '3.1.0',
+	'version' => '3.2.0',
 	'dependencies' => '',
 	'conflicts' => 'speedy,queo_speedup,js_css_optimizer,minify',
 	'priority' => 'bottom',
@@ -46,7 +46,7 @@ $EM_CONF[$_EXTKEY] = array(
 		'suggests' => array(
 		),
 	),
-	'_md5_values_when_last_written' => 'a:15:{s:25:"class.tx_scriptmerger.php";s:4:"2c02";s:31:"class.tx_scriptmerger_cache.php";s:4:"717d";s:16:"example.htaccess";s:4:"c522";s:12:"ext_icon.gif";s:4:"2cfb";s:17:"ext_localconf.php";s:4:"1455";s:14:"ext_tables.php";s:4:"fa00";s:27:"configuration/constants.txt";s:4:"371a";s:23:"configuration/setup.txt";s:4:"23c8";s:14:"doc/manual.sxw";s:4:"b855";s:23:"resources/jsminplus.php";s:4:"48ed";s:35:"resources/minify/lib/Minify/CSS.php";s:4:"dc9e";s:48:"resources/minify/lib/Minify/CommentPreserver.php";s:4:"86ba";s:47:"resources/minify/lib/Minify/ImportProcessor.php";s:4:"5ce5";s:46:"resources/minify/lib/Minify/CSS/Compressor.php";s:4:"b514";s:47:"resources/minify/lib/Minify/CSS/UriRewriter.php";s:4:"5ab5";}',
+	'_md5_values_when_last_written' => 'a:15:{s:25:"class.tx_scriptmerger.php";s:4:"6dc9";s:31:"class.tx_scriptmerger_cache.php";s:4:"ac45";s:16:"example.htaccess";s:4:"7469";s:12:"ext_icon.gif";s:4:"2cfb";s:17:"ext_localconf.php";s:4:"1455";s:14:"ext_tables.php";s:4:"9927";s:27:"configuration/constants.txt";s:4:"038d";s:23:"configuration/setup.txt";s:4:"5fdd";s:14:"doc/manual.sxw";s:4:"4c18";s:23:"resources/jsminplus.php";s:4:"13d4";s:35:"resources/minify/lib/Minify/CSS.php";s:4:"388d";s:48:"resources/minify/lib/Minify/CommentPreserver.php";s:4:"9762";s:47:"resources/minify/lib/Minify/ImportProcessor.php";s:4:"16f4";s:46:"resources/minify/lib/Minify/CSS/Compressor.php";s:4:"92c8";s:47:"resources/minify/lib/Minify/CSS/UriRewriter.php";s:4:"d1db";}',
 	'suggests' => array(
 	),
 );
