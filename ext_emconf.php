cb97613ffc1ef665b9a90cada107255c4c114540
sgalinsk <sgalinsk@735d13b6-9817-0410-8766-e36946ffe9aa>
2011-11-23 23:35:51 +0000

    [FEATURE] 4.6 compatibility
    git-svn-id: https://svn.typo3.org/TYPO3v4/Extensions/scriptmerger/trunk@54773 735d13b6-9817-0410-8766-e36946ffe9aa

--- ext_emconf.php
+++ ext_emconf.php
@@ -35,7 +35,7 @@ $EM_CONF[$_EXTKEY] = array(
 	'constraints' => array(
 		'depends' => array(
 			'php' => '5.2.1-5.3.99',
-			'typo3' => '4.2.0-4.5.99',
+			'typo3' => '4.2.0-4.6.99',
 		),
 		'conflicts' => array(
 			'speedy' => '',
