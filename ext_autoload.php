cab321c157414bb2e8b318bf690b6cd17518c384
sgalinsk <sgalinsk@735d13b6-9817-0410-8766-e36946ffe9aa>
2013-05-15 18:08:44 +0000

    [BUGFIX] Autoload files must return an empty array
    git-svn-id: https://svn.typo3.org/TYPO3v4/Extensions/scriptmerger/trunk@75384 735d13b6-9817-0410-8766-e36946ffe9aa

--- ext_autoload.php
+++ ext_autoload.php
@@ -3,8 +3,8 @@
 // Can be activated as far as TYPO3 4.5 support is dropped
 
 //$extensionPath = t3lib_extMgm::extPath('scriptmerger');
-//
-//return array(
+
+return array(
 //	'minify_importprocessor' => $extensionPath . 'resources/minify/lib/Minify/ImportProcessor.php',
 //	'minify_commentpreserver' => $extensionPath . 'resources/minify/lib/Minify/CommentPreserver.php',
 //	'minify_css' => $extensionPath . 'resources/minify/lib/Minify/CSS.php',
@@ -12,6 +12,6 @@
 //	'minify_css_urirewriter' => $extensionPath . 'resources/minify/lib/Minify/CSS/UriRewriter.php',
 //	'jsminplus' => $extensionPath . 'resources/jsminplus.php',
 //	'jsmin' => $extensionPath . 'resources/jsmin.php',
-//);
+);
 
 ?>
