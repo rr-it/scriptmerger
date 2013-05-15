1cd639f921a0e1234d327bdc37249a01d1135051
sgalinsk <sgalinsk@735d13b6-9817-0410-8766-e36946ffe9aa>
2013-05-15 18:06:06 +0000

    [TASK] Increase version requirements
    git-svn-id: https://svn.typo3.org/TYPO3v4/Extensions/scriptmerger/trunk@75383 735d13b6-9817-0410-8766-e36946ffe9aa

--- ext_emconf.php
+++ ext_emconf.php
@@ -34,8 +34,8 @@ $EM_CONF[$_EXTKEY] = array(
 	'CGLcompliance_note' => '',
 	'constraints' => array(
 		'depends' => array(
-			'php' => '5.2.1-5.3.99',
-			'typo3' => '4.2.0-4.6.99',
+			'php' => '5.2.1-5.4.99',
+			'typo3' => '4.5.0-6.1.99',
 		),
 		'conflicts' => array(
 			'speedy' => '',
