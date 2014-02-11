0304f39e642037b8e262afe964ffdcf15ef77b6b
sgalinsk <sgalinsk@735d13b6-9817-0410-8766-e36946ffe9aa>
2014-02-11 14:37:39 +0000

    [TASK] Update the php constraint (5.3 is at least required now!)
    git-svn-id: https://svn.typo3.org/TYPO3v4/Extensions/scriptmerger/trunk@83716 735d13b6-9817-0410-8766-e36946ffe9aa

--- ext_emconf.php
+++ ext_emconf.php
@@ -34,7 +34,7 @@ $EM_CONF[$_EXTKEY] = array(
     'CGLcompliance_note' => '',
     'constraints' => array(
         'depends' => array(
-            'php' => '5.2.1-5.4.99',
+            'php' => '5.3.0-5.5.99',
             'typo3' => '4.5.0-6.1.99',
         ),
         'conflicts' => array(
