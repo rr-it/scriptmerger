e26b7aa53532c2b26984e6f21ad665226acf6e6d
Fabian Galinski <fabian@sgalinski.de>
2015-12-01 20:40:47 +0100

    [BUGFIX] The extension dosen't works with TYPO3 6.2

--- ext_localconf.php
+++ ext_localconf.php
@@ -7,14 +7,14 @@ if (!defined('TYPO3_MODE')) {
 // post processing hook to clear any existing cache files if the button in
 // the backend is clicked (contains an age check)
 $GLOBALS['TYPO3_CONF_VARS']['SC_OPTIONS']['t3lib/class.t3lib_tcemain.php']['clearCachePostProc'][] =
-	'EXT:scriptmerger/Classes/CacheHook.php:SGalinski\Scriptmerger\user_ScriptmergerCacheHook->clearCachePostProc';
+	'EXT:scriptmerger/Classes/user_ScriptmergerCacheHook.php:SGalinski\Scriptmerger\user_ScriptmergerCacheHook->clearCachePostProc';
 
 // register the minify, compress and merge processes
 if (TYPO3_MODE == 'FE') {
 	$TYPO3_CONF_VARS['SC_OPTIONS']['tslib/class.tslib_fe.php']['contentPostProc-output'][] =
-		'EXT:scriptmerger/Classes/OutputHook.php:SGalinski\Scriptmerger\user_ScriptmergerOutputHook->contentPostProcOutput';
+		'EXT:scriptmerger/Classes/user_ScriptmergerOutputHook.php:SGalinski\Scriptmerger\user_ScriptmergerOutputHook->contentPostProcOutput';
 	$TYPO3_CONF_VARS['SC_OPTIONS']['tslib/class.tslib_fe.php']['contentPostProc-all'][] =
-		'EXT:scriptmerger/Classes/OutputHook.php:SGalinski\Scriptmerger\user_ScriptmergerOutputHook->contentPostProcAll';
+		'EXT:scriptmerger/Classes/user_ScriptmergerOutputHook.php:SGalinski\Scriptmerger\user_ScriptmergerOutputHook->contentPostProcAll';
 }
 
 // needs to be disabled for the frontend, otherwise the default exclude rule prevents any script files from merging
