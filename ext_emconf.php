a551595772bea32db51fff55bddb219f6e756b3e
Paul Ilea <paul@sgalinski.de>
2017-05-29 11:11:57 +0300

    [TASK] Update typo3 version requirement

--- ext_emconf.php
+++ ext_emconf.php
@@ -27,7 +27,7 @@ $EM_CONF[$_EXTKEY] = array (
 		'depends' =>
 		array (
 			'php' => '5.3.0-5.6.99',
-			'typo3' => '6.2.0-7.6.99',
+			'typo3' => '6.2.0-8.7.99',
 		),
 		'conflicts' =>
 		array (
